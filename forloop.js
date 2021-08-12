7c24955252b6c8590080cb14db9295d0cd2307e5
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-08-12 12:03:35 +0530

    added a bunch of files

---
+++ forloop.js
@@ -0,0 +1,14 @@
+
+    <script>
+        var x;
+        for(x=0;x<=5;x++)
+        {
+            document.write("value -",x,"</br>");
+        }
+    </script>
+    <script>
+        for(x in navigator)
+        {
+            document.write(x,"</br>");
+        }
+    </script>
---
+++ loopingstatement.js
@@ -0,0 +1,13 @@
+
+    <script type="text/javascript">
+    var x = 0;
+    var y = 0;
+    while(x<=10)
+    {
+        document.write("value-",x,"</br>");x++;
+    }
+    do{
+        document.write(y);y++;
+    }
+    while(y<5)
+    </script>
