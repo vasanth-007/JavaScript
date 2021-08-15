503e18db37302ac6895663ea8eecf80ab0b798a6
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-08-15 20:07:50 +0530

    added a files

---
+++ for.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+    <script type="text/javascript">
+    var x;
+    for(x=0;x<10;x++)
+    {
+        document.write(x);
+    }
+    for(x in navigator)
+    {
+        document.write(x,"</br>")
+    }
+    </script>
+</body>
