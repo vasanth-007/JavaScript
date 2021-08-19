44cc9ce9f4fca7487ff6debf7243f4bb926cb932
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-08-19 16:40:02 +0530

    added a file

---
+++ objects.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head></head>
+<body>
+    <script type="text/javascript">
+    var records = new Object();
+    records.name = "vasanth";
+    records.age = "20";
+    document.write(records.name + records.age);
+    </script>
+</body>
+</html>
