e67e8dd1b3b779ecf81ebdfa67084464e94b54b6
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-08-26 13:31:54 +0530

    added a file

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 id="title"></h1>
+    <script type="text/javascript">
+    let firstName = "vasanth"; //string
+    let age = 20; //number
+    let isnot = false; //boolean
+    let name; //undefined
+    let trophy = null; //null
+    document.write("string:"+ firstName,"</br>","number:" + age,"</br>","boolean:"+ isnot,"</br>","name; = "+ name,"</br>","trophy = null;");
+    document.getElementById("title").innerHTML = "data types";
+    </script>
+</body>
+</html>
