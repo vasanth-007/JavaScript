6f63638892d04129bd30978ecc51db473c68d50d
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-09-13 18:51:18 +0530

    added a file

---
+++ precedence.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script type="text/javascript">
+        var a = 5;
+        var b = 2;               
+        var c = 10;
+        document.write ((a+b)*c);   //operator precedence
+    </script> 
+</body>
+</html>
