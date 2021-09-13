6c3ee830059271fb71ff54cbccfaf72e887207f7
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-09-13 18:51:56 +0530

    added a file

---
+++ operators.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--arthmatic-->
+    <!--addition-->
+    <script type="text/javascript"> 
+    var x = 5;
+    var y = 10;
+    document.write("addition:",x+y,"</br>");
+    </script>
+    <!--subration-->
+    <script type="text/javascript"> 
+    var x = 5;
+    var y = 10;
+    document.write("subraction :",x-y,"</br>");
+    </script>
+    <!--divition-->
+    <script type="text/javascript"> 
+    var x = 5;
+    var y = 10;
+    document.write("modulus :",x%y,"</br>");
+    </script>
+    <!--increament-->
+    <script type="text/javascript"> 
+    var x = 5;
+    var y = 10;
+    document.write("increment :",x--,"</br>");
+    document.write("incremented value :",x);
+    </script>
+</body>
+</html>
