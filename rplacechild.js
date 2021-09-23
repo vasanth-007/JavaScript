a87a3d5afd0d96dba26ea047f40d06c2be98bb32
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-09-23 21:41:21 +0530

    added a file

---
+++ rplacechild.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<div class="parent">
+<p class="p1">hello</p>
+<p class="p2">world</p>
+</div>
+<script type="text/javascript">
+    document.getElementsByClassName("p1")[0].style.color = "blue"; //class doent returm an elemnt instead ut return node list so us 
+</script>
+</body>
+</html>
