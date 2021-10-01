4c4cca07607c679ca351edee0bba9da5c76f1a30
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-10-01 20:31:31 +0530

    added a file

---
+++ joinSplit.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script type="text/javascript">
+    const dailyRoutine = ["wakup","eat","learn","sleep"]
+
+    let daily = dailyRoutine.join(", ");
+    console.log(daily);
+    </script>
+    <script type="text/javascript">
+    let fullName = "nazriya fahad";
+
+    let final = fullName.split(" ");
+
+    let firstName = final[0];
+    let lastName = final[1];
+
+    console.log("my first name is " +firstName+ "last name is " +lastName );
+    console.log(final);
+    </script>
+</body>
+</html>
