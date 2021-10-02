04a21ace6a8b71cd9c7c9c887299903956f535d5
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-10-02 21:11:48 +0530

    added a file

---
+++ map.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script type="text/javascript"> 
+    const numbers = [1,2,3,4,5]
+    console.log(numbers);
+    let total = numbers.map((val)=>val * 2)
+    console.log(total);
+    </script>
+</body>
+</html>
