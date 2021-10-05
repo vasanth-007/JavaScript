8d31c154d88db62eb37249099ac0f4036086fde7
VASANTHARAMAN G <vasanthreigns5306@gmail.com>
2021-10-05 21:38:45 +0530

    added a file

---
+++ masteringinfunction.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script type="text/javascript">
+    //function 
+    
+
+    //function exprssion
+   
+
+    //imvoked function
+    (function addno(myE){
+        let no1 = 10;
+        let no2 = 20;
+        let total = no1+no2;
+       let msg = "my total expansive is " + myE;
+       console.log(total);
+       console.log(msg);
+    })(9000);
+    </script>
+</body>
+</html>
